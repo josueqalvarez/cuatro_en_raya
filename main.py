cbcb2293af8b6d23297e5e4406e2c2adf0b989c5
Josue_celular <quichcajosue@gmail.com>
2026-05-23 08:27:06 -0500

    Avance del grafico en terminal

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from crear_jugador import Jugador
+from tablero import Tablaero
 
 jugador_1 = Jugador()
 jugador_1.definir_datos()
 
-print(jugador_1.nombre)
+jugador_2 = Jugador()
+jugador_2.definir_datos()
+
+tablero = Tablero()
+
+print(f"Rojos empiezan. \nTurno de {jugador_1.nombre}")
+
+
+tablero.mostrar()
--- tablero.py
+++ tablero.py
@@ -1,10 +1,12 @@
 class Tablero:
     def __init__(self, filas = 6, columnas = 7):
         self.columnas = columnas
-        self.filad = filas
+        self.filas = filas
 
     def mostrar(self):
-        for col in range(self.columnas + 1):
-            for fil in range(self.filas):
-                print(0)
-            print('\n')
+        for fil in range(self.filas):
+            fila_tablero = ""
+            for col in range(self.columnas):
+                fila_tablero = fila_tablero + "⚪️ "
+            print(f'{fila_tablero}\n')
+
